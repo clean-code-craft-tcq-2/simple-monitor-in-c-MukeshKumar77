6e5cbbaca777c3cb119e95fb41da88b0704ed2cd
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-02-12 19:27:03 +0530

    Create BMS_SOC_Checker.c

---
+++ BMS_SOC_Checker.c
@@ -0,0 +1,27 @@
+#include "BMS_module.h"
+
+const char* SoCMessage[2] = {"State of Charge is out of range" , "Der Ladezustand ist außerhalb des Bereichs"};
+
+void isStateOfChargeWithinLimit(BatteryInfo batteryData)
+{
+	// set the unit to default
+	const char* UnitofSOC[] = {"%"};
+	unsigned char defaultUnit = 1;
+
+	unsigned char errorStatus;
+	errorStatus = BatteryErrorStatus;
+	if(batteryData.batteryStateOfCharge < 20)
+	{
+		errorStatus |= LOW_STATE_OF_CHARGE;
+		printWarningToConsole(SoCMessage, UnitofSOC, batteryData.batteryStateOfCharge,
+								defaultUnit, LanguageSelection);
+	}
+	else if(batteryData.batteryStateOfCharge > 80)
+	{
+		errorStatus |= HIGH_STATE_OF_CHARGE;
+		printWarningToConsole(SoCMessage, UnitofSOC, batteryData.batteryStateOfCharge,
+								defaultUnit, LanguageSelection);
+	}
+	BatteryErrorStatus = errorStatus;
+}
+
