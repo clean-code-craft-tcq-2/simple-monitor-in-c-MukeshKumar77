030a14b2f6114ea4e49c7581cf91303551cc5b39
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-02-13 00:48:53 +0530

    Update BMS_Temperature_Checker.c

--- BMS_Temperature_Checker.c
+++ BMS_Temperature_Checker.c
@@ -1,14 +1,17 @@
 #include "BMS_module.h"
 
-const char* temperatureMessage[2] = {"Temperature  is out of range" , "Die Temperatur liegt außerhalb des zulässigen Bereichs"};
+const char* temperatureMessage[5][2] = {
+		{"Error: Temperature is below the range" , "Fehler: Temperatur liegt unterhalb des Bereichs"},
+		{"Warning: Temperature is too low " , "Warnung: Die Temperatur ist zu niedrig"},
+		{"Temperature is normal" , "Temperatur ist normal"},
+		{"Warning: Temperature is too high" , "Warnung: Die Temperatur ist zu hoch"},
+		{"Error: Temperature is above the range" , "Fehler: Temperatur liegt über dem Bereich"}};
+
+float fahrenheitTemp;
 
 void convertDataToStandardUnit(BatteryInfo* data, unsigned char selectedUnit)
 {
-	if((selectedUnit & TEMPERATURE_IN_CELSIUS) == TEMPERATURE_IN_CELSIUS)
-	{
-		//do nothing
-	}
-	else if ((selectedUnit & TEMPERATURE_IN_FAHRENHEIT) == TEMPERATURE_IN_FAHRENHEIT)
+	if ((selectedUnit & TEMPERATURE_IN_FAHRENHEIT) == TEMPERATURE_IN_FAHRENHEIT)
 	{
 		data->batteryTemperature = ((data->batteryTemperature - 32) * 5 / 9);
 	}
@@ -17,26 +20,19 @@ void convertDataToStandardUnit(BatteryInfo* data, unsigned char selectedUnit)
 void isBatteryTemperatureWithinLimit(BatteryInfo batteryData)
 {
 	//take a copy of temperature to print at console
-	float temperature = batteryData.batteryTemperature;
+	fahrenheitTemp = batteryData.batteryTemperature;
 
 	convertDataToStandardUnit(&batteryData, BatteryDataUnitSelection);
 
 	unsigned char errorStatus;
 	errorStatus = BatteryErrorStatus;
-	if(batteryData.batteryTemperature < 0)
+	errorStatus = ((checkForLimit(temperatureMessage, UnitofBatteryData, batteryData.batteryTemperature, BatteryDataUnitSelection, LOW_WARNING_MESSAGE, LOW_TEMPERATURE_WARNING)) ||
+					(checkForLimit(temperatureMessage, UnitofBatteryData, batteryData.batteryTemperature, BatteryDataUnitSelection, NORMAL_MESSSAGE, NORMAL_TEMPERATURE)) ||
+					(checkForLimit(temperatureMessage, UnitofBatteryData, batteryData.batteryTemperature, BatteryDataUnitSelection, HIGH_WARNING_MESSAGE, HIGH_TEMPERATURE_WARNING)));
+	if(!errorStatus)
 	{
-		errorStatus |= LOW_BATTERY_TEMPERATURE;
-		batteryData.batteryTemperature = temperature;
-		printWarningToConsole(temperatureMessage, UnitofBatteryData, batteryData.batteryTemperature,
-								BatteryDataUnitSelection, LanguageSelection);
+		errorStatus = ((checkForLimit(temperatureMessage, UnitofBatteryData, batteryData.batteryTemperature, BatteryDataUnitSelection, LOW_BREACH_MESSAGE, LOW_TEMPERATURE_BREACH)) ||
+						(checkForLimit(temperatureMessage, UnitofBatteryData, batteryData.batteryTemperature, BatteryDataUnitSelection, HIGH_BREACH_MESSAGE, HIGH_TEMPERATURE_BREACH)));
+		BatteryErrorStatus = errorStatus;
 	}
-	else if(batteryData.batteryTemperature > 45)
-	{
-		errorStatus |= HIGH_BATTERY_TEMPERATURE;
-		batteryData.batteryTemperature = temperature;
-		printWarningToConsole(temperatureMessage, UnitofBatteryData, batteryData.batteryTemperature,
-								BatteryDataUnitSelection, LanguageSelection);
-	}
-	BatteryErrorStatus = errorStatus;
 }
-
