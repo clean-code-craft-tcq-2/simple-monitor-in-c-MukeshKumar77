d4ca1e09dc5038a0d8448392925d16021befe852
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-02-13 01:14:31 +0530

    Update BMS_ChargeRate_Checker.c

--- BMS_ChargeRate_Checker.c
+++ BMS_ChargeRate_Checker.c
@@ -7,17 +7,27 @@ const char* chargeRateMessage[5][2] = {
 		{"Warning: Charge Rate is too high" , "Warnung: Die Ladestrom ist zu hoch"},
 		{"Error: Charge Rate is above the range" , "Fehler: Ladestrom liegt über dem Bereich"}};
 
+// set the unit to default
+const char* UnitofChargeRate[] = {"A"};
+static unsigned char defaultUnitIdx = 1;
+
+static bool checkForChargeRateWarningFirst(float parameter);
+
+static bool checkForChargeRateWarningFirst(float parameter)
+{
+	bool retVal;
+	retVal = ((checkForLimit(chargeRateMessage, UnitofChargeRate, parameter, defaultUnitIdx, LOW_WARNING_MESSAGE, LOW_CHARGERATE_WARNING)) ||
+				(checkForLimit(chargeRateMessage, UnitofChargeRate, parameter, defaultUnitIdx, NORMAL_MESSSAGE, NORMAL_CHARGERATE)) ||
+				(checkForLimit(chargeRateMessage, UnitofChargeRate, parameter, defaultUnitIdx, HIGH_WARNING_MESSAGE, HIGH_CHARGERATE_WARNING)));
+	return(retVal);
+}
+
 void isChargeRateWithinLimit(BatteryInfo batteryData)
 {
-	// set the unit to default
-	const char* UnitofChargeRate[] = {"A"};
-	unsigned char defaultUnitIdx = 1;
-
 	unsigned char errorStatus;
 	errorStatus = BatteryErrorStatus;
-	errorStatus = ((checkForLimit(chargeRateMessage, UnitofChargeRate, batteryData.batteryChargeRate, defaultUnitIdx, LOW_WARNING_MESSAGE, LOW_CHARGERATE_WARNING)) ||
-					(checkForLimit(chargeRateMessage, UnitofChargeRate, batteryData.batteryChargeRate, defaultUnitIdx, NORMAL_MESSSAGE, NORMAL_CHARGERATE)) ||
-					(checkForLimit(chargeRateMessage, UnitofChargeRate, batteryData.batteryChargeRate, defaultUnitIdx, HIGH_WARNING_MESSAGE, HIGH_CHARGERATE_WARNING)));
+	errorStatus = checkForChargeRateWarningFirst(batteryData.batteryChargeRate);
+
 	if(!errorStatus)
 	{
 		errorStatus = ((checkForLimit(chargeRateMessage, UnitofChargeRate, batteryData.batteryChargeRate, defaultUnitIdx, LOW_BREACH_MESSAGE, LOW_CHARGERATE_BREACH)) ||
