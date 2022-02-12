6d05b73456beb8c47ae4974506d01c9f727a00e6
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-02-13 00:49:16 +0530

    Update BMS_SOC_Checker.c

--- BMS_SOC_Checker.c
+++ BMS_SOC_Checker.c
@@ -1,6 +1,11 @@
 #include "BMS_module.h"
 
-const char* SoCMessage[2] = {"State of Charge is out of range" , "Der Ladezustand ist außerhalb des Bereichs"};
+const char* SoCMessage[5][2] = {
+		{"Error: SOC is below the range" , "Fehler: SOC liegt unterhalb des Bereichs"},
+		{"Warning: SOC approaching discharge " , "Warnung: SOC nähert sich der Entladung"},
+		{"SOC is normal" , "SOC ist normal"},
+		{"Warning: SOC is approaching peak charge" , "Warnung: SOC nähert sich der Spitzenladung"},
+		{"Error: SOC is above the range" , "Fehler: SOC liegt über dem Bereich"}};
 
 void isStateOfChargeWithinLimit(BatteryInfo batteryData)
 {
@@ -10,18 +15,13 @@ void isStateOfChargeWithinLimit(BatteryInfo batteryData)
 
 	unsigned char errorStatus;
 	errorStatus = BatteryErrorStatus;
-	if(batteryData.batteryStateOfCharge < 20)
+	errorStatus = ((checkForLimit(SoCMessage, UnitofSOC, batteryData.batteryStateOfCharge, defaultUnitIdx, LOW_WARNING_MESSAGE, LOW_SOC_WARNING)) ||
+					(checkForLimit(SoCMessage, UnitofSOC, batteryData.batteryStateOfCharge, defaultUnitIdx, NORMAL_MESSSAGE, NORMAL_SOC)) ||
+					(checkForLimit(SoCMessage, UnitofSOC, batteryData.batteryStateOfCharge, defaultUnitIdx, HIGH_WARNING_MESSAGE, HIGH_SOC_WARNING)));
+	if(!errorStatus)
 	{
-		errorStatus |= LOW_STATE_OF_CHARGE;
-		printWarningToConsole(SoCMessage, UnitofSOC, batteryData.batteryStateOfCharge,
-								defaultUnitIdx, LanguageSelection);
+		errorStatus = ((checkForLimit(SoCMessage, UnitofSOC, batteryData.batteryStateOfCharge, defaultUnitIdx, LOW_BREACH_MESSAGE, LOW_SOC_BREACH)) ||
+						(checkForLimit(SoCMessage, UnitofSOC, batteryData.batteryStateOfCharge, defaultUnitIdx, HIGH_BREACH_MESSAGE, HIGH_SOC_BREACH)));
+		BatteryErrorStatus = errorStatus;
 	}
-	else if(batteryData.batteryStateOfCharge > 80)
-	{
-		errorStatus |= HIGH_STATE_OF_CHARGE;
-		printWarningToConsole(SoCMessage, UnitofSOC, batteryData.batteryStateOfCharge,
-								defaultUnitIdx, LanguageSelection);
-	}
-	BatteryErrorStatus = errorStatus;
 }
-
