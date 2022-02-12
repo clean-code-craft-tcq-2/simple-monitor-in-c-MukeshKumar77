c95bba7392ebea3a524405c6b9790e490e566e5e
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-02-13 00:50:11 +0530

    Update PrintWarningToConsole.c

--- PrintWarningToConsole.c
+++ PrintWarningToConsole.c
@@ -1,8 +1,11 @@
 #include "BMS_module.h"
 
-void printWarningToConsole(const char *warningMessage[], const char *unitInfo[],
-		                        float batteryData, unsigned char unit, unsigned char language)
+void printWarningToConsole(const char *warningMessage[5][2],
+							const char *unitInfo[],
+							float batteryData,
+							unsigned char UnitIndex,
+							unsigned char MessageIndex,
+							unsigned char Language)
 {
-	printf("%.1f %s --> %s\n", batteryData, unitInfo[unit - 1], warningMessage[language - 1]);
+	printf("%.1f %s --> %s\n", batteryData, unitInfo[UnitIndex - 1], warningMessage[MessageIndex][Language - 1]);
 }
-
